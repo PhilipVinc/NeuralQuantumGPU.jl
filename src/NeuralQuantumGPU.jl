f56a54a0bee73fe26728af4896260e583d03e5f1
Filippo Vicentini <filippovicentini@gmail.com>
2019-10-09 18:44:03 +0200

    2

--- src/NeuralQuantumGPU.jl
+++ src/NeuralQuantumGPU.jl
@@ -2,7 +2,7 @@ module NeuralQuantumGPU
 
 using CuArrays: CuArrays, @cufunc
 using CuArrays: CuArrays.GPUArrays.GPUArray
-using NeuralQuantum: NeuralQuantum, State, _std_state_batch
+using NeuralQuantum: NeuralQuantum, State, _std_state_batch, store_state!
 using UnsafeArrays
 using NNlib
 
@@ -10,14 +10,28 @@ using NNlib
 
 @cufunc NeuralQuantum.ℒ(x) = one(x) + exp(x)
 @cufunc NeuralQuantum.∂logℒ(x) = one(x)/(one(x)+exp(-x))
-@cufunc NeuralQuantum.logℒ(x::Real) = log1p(exp(x))
-@cufunc NeuralQuantum.logℒ(x::Complex) = log(one(x) + exp(x))
+
+_gpu_logℒ(x) = log1p(exp(x))
+@cufunc _gpu_logℒ(x::Real) = log1p(exp(x))
+@cufunc _gpu_logℒ(x::Complex) = log(one(x) + exp(x))
+
+@cufunc NeuralQuantum.logℒ(x) = _gpu_logℒ(x)
+
 
 struct CPUCachedBatchState{G,V} <: NeuralQuantum.State
     gpu_state::G
     cpu_state::V
 end
-NeuralQuantum.config(v::CPUCachedBatchState) = copy!(v.gpu_state, v.cpu_state)
+NeuralQuantum.config(v::CPUCachedBatchState) = _copy_to_gpu!(v.gpu_state, v.cpu_state)
+
+_copy_to_gpu!(g::AbstractArray, v::AbstractArray) = copy!(g, v)
+_copy_to_gpu!(g::Tuple, c::Tuple) = begin
+    g1, g2 = g
+    c1, c2 = c
+    copy!(g1, c1)
+    copy!(g2, c2)
+    return g
+end
 
 NeuralQuantum.preallocate_state_batch(arrT::GPUArray,
                                       T::Type{<:Real},
