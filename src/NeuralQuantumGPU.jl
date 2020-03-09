2c7034ee0a734c65a775f40a3dae154e4d3f60da
Filippo Vicentini <filippovicentini@gmail.com>
2020-03-09 19:02:45 +0100

    fl

--- src/NeuralQuantumGPU.jl
+++ src/NeuralQuantumGPU.jl
@@ -9,32 +9,16 @@ using NNlib
 gpu(x) = use_cuda[] ? fmap(CuArrays.cu, x) : x
 
 # gpu stuff
+include("CuArrays/cuda.jl")
+include("CuArrays/upstream.jl")
 
 function __init__()
-
-  # cuda stuff
-  precompiling = ccall(:jl_generating_output, Cint, ()) != 0
-
-  # we don't want to include the CUDA module when precompiling,
-  # or we could end up replacing it at run time (triggering a warning)
-  precompiling && return
-
-  if !CuArrays.functional()
-    # nothing to do here, and either CuArrays or one of its dependencies will have warned
-  else
-    use_cuda[] = true
-    include("CuArrays/cuda.jl")
-    include("CuArrays/upstream.jl")
-
-    # FIXME: this functionality should be conditional at run time by checking `use_cuda`
-    #        (or even better, get moved to CuArrays.jl as much as possible)
-    if CuArrays.has_cudnn()
-      #include(joinpath(@__DIR__, "cuda/cuda.jl"))
-    else
-      @warn "CuArrays.jl did not find libcudnn. Some functionality will not be available."
+  use_cuda[] = CuArrays.functional() # Can be overridden after load with `Flux.use_cuda[] = false`
+  if CuArrays.functional()
+    if !CuArrays.has_cudnn()
+      @warn "CuArrays.jl found cuda, but did not find libcudnn. Some functionality will not be available."
     end
   end
 end
-
 
 end # module
