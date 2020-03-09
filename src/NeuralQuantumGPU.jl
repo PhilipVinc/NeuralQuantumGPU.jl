85acf9bab851752be96fb613b595c71cc78fc4f8
Filippo Vicentini <filippovicentini@gmail.com>
2020-03-09 19:05:19 +0100

    no message

--- src/NeuralQuantumGPU.jl
+++ src/NeuralQuantumGPU.jl
@@ -1,7 +1,7 @@
 module NeuralQuantumGPU
 
 using CuArrays: CuArrays, CuArray, @cufunc, CUBLAS
-const use_cuda = Ref(false)
+const use_cuda = Ref(true)
 
 using NeuralQuantum: NeuralQuantum
 using NNlib
@@ -11,14 +11,5 @@ gpu(x) = use_cuda[] ? fmap(CuArrays.cu, x) : x
 # gpu stuff
 include("CuArrays/cuda.jl")
 include("CuArrays/upstream.jl")
-
-function __init__()
-  use_cuda[] = CuArrays.functional() # Can be overridden after load with `Flux.use_cuda[] = false`
-  if CuArrays.functional()
-    if !CuArrays.has_cudnn()
-      @warn "CuArrays.jl found cuda, but did not find libcudnn. Some functionality will not be available."
-    end
-  end
-end
 
 end # module
